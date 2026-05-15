4c0059f7945c8115077621cb290e128c2aedeea0
luciagr-2026 <luciagroldan@alumnos.cei.es>
2026-05-15 19:48:19 +0000

    Version 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 const OffScreenMenu = document.querySelector(`.Off-screen-menu`)
 console.log(OffScreenMenu)
 
@@ -12,5 +11,21 @@ HeaderBurger.addEventListener((`click`) , () => {
 
     OffScreenMenu.classList.toggle(`Active`)
 
-    
 })
+
+
+const OffScreenMenuSpan = document.querySelector(`.Off-screen-menu-span`)
+console.log(OffScreenMenuSpan)
+
+OffScreenMenuSpan.addEventListener((`click`) , () => {
+
+OffScreenMenuSpan.classList.toggle(`Active`)
+
+OffScreenMenuSpan.addEventListener((`click`) , () => {
+
+    OffScreenMenu.classList.remove(`Active`)
+})
+
+})
+
+
